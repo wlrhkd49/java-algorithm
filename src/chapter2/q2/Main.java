b6a34e5d9b810940c9471837f05a04b8a26abee6
yeojin <>
2025-07-25 16:11:57 +0900

    test

---
+++ src/chapter2/q2/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void solution(String str) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+    }
+}
